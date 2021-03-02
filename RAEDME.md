02aa75b47a14c7a3dd855fb9ff007ddcd9e03a8a
João Bezerra Leite <joao.leite@restoque.com.br>
2021-03-02 19:25:28 -0300

    feat: Criação das migrations

--- RAEDME.md
+++ RAEDME.md
@@ -6,6 +6,9 @@ $ yarn dev
 executar as migrations:
 $ yarn typeorm migration:run
 
+Criar migrations:
+$ yarn typeorm migration:create -n CreateUser
+
 realizar Teste
 $ yarn test
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,18 +4,27 @@ import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
 
-   async create( request: Request, response: Response){
+   async create(request: Request, response: Response){
       const { name, email } = request.body;
 
       const usersRepository = getCustomRepository(UsersRepository)
 
       const user = usersRepository.create({
-         name, email
+         name,
+         email
       });
 
       await usersRepository.save(user);
 
       return response.status(201).json(user);
+   }
+
+   async show(request: Request, response: Response){
+      const usersRepository = getCustomRepository(UsersRepository);
+
+      const all = await usersRepository.find();
+
+      return response.json(all);
    }
 
 }
